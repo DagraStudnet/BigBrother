6127d6f82a80abbd986b59589fa18ba805427ae1
Lukáš Rajm <lukas.rajm@unicorn.eu>
2015-07-25 21:07:10 +0200

    oprava presunu

--- HostingBigBrother/ViewModel/ViewModelMain.cs
+++ HostingBigBrother/ViewModel/ViewModelMain.cs
@@ -1,12 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Threading;
 using ClassLibrary;
-using UserStorageLibrary;
 using HostingBigBrother.Model;
+using UserStorageLibrary;
 
 namespace HostingBigBrother.ViewModel
 {
--- WcfServiceLibrary/Library.cs
+++ WcfServiceLibrary/Library.cs
@@ -2,6 +2,7 @@
 using ClassLibrary;
 using UserStorageLibrary;
 
+
 namespace WcfServiceLibrary
 {
     // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
